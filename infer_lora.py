ced337c755dc3e078a7699819d599d846c87bf01
Benson <benson.mwangi@gmail.com>
2025-05-02 16:36:34 -0500

    inference sample -bug fix -edit infer lora

--- infer_lora.py
+++ infer_lora.py
@@ -256,7 +256,10 @@ def run():
             local_path, seed = run_one_case(pipe, **example)
 
     else:
-        assert cfg.args.task_type.upper() in task_model_cfg
+        print('DEBUG: task_model_cfg keys:', list(task_model_cfg.keys()))
+        print('DEBUG: task_model_cfg.MODEL keys:', list(task_model_cfg.MODEL.keys()))
+        print('DEBUG: cfg.args.task_type.upper():', cfg.args.task_type.upper())
+        assert cfg.args.task_type.upper() in task_model_cfg.MODEL
         params = {
             "input_image": cfg.args.input_image,
             "input_mask": cfg.args.input_mask,
